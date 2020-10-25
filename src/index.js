68c25b0a86c5be273124fd4d43950cee95dce666
malanka96 <voksa.kolya@gmail.com>
2020-10-25 10:14:15 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+  module.exports = function check(str, bracketsConfig) {
+  let lgStr = str.length-1;
+  if (str.length%2 == 0) {
+    for (let i = lgStr; i >= 0; i--) {
+      for (let a = 0; a < bracketsConfig.length; a++) {
+       let b = bracketsConfig[a].join('');
+        if (str[i] == bracketsConfig[a][0]) str = str.replace(b, '');
+      }
+    }
+    let result = (str != '') ? false : true;
+    return result;
+  } else { return false; }
 }
